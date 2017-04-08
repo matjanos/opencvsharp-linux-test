480990e21f65a9e36c4bd6e8146f8dd667fa3070
Kuba Matjanowski <k.matjanowski@gmail.com>
2017-04-08 20:32:56 +0200

    create simple app

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenCvSharp;
+using System;
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var test = new Mat();
+        }
+    }
+}
