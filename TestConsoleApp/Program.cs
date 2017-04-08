bfb31fd2ba8c9c6c436183d0e863ab2bfe383147
Kuba Matjanowski <k.matjanowski@gmail.com>
2017-04-08 21:04:50 +0200

    added more info, copy Extern files for Windows, reduced Docker file

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -7,7 +7,21 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var test = new Mat();
+            try
+            {
+                var test = new Mat();
+                Console.WriteLine("Woooo hoooo! You managed to run OpenCVSharp here!");
+            }
+            catch (TypeInitializationException e)
+            {
+                Console.WriteLine("It still crashes... ;(");
+                Console.WriteLine(e.InnerException.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Hold on! It still crashes, but an exception is different.");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
